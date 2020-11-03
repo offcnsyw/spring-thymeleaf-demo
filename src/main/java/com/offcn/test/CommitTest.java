09181d62c3eaffccb1ba42c7e8a887365cde7a73
syw <songyinwei@163.com>
2020-11-03 15:12:19 +0800

    分支测试

--- src/main/java/com/offcn/test/CommitTest.java
+++ src/main/java/com/offcn/test/CommitTest.java
@@ -3,5 +3,6 @@ package com.offcn.test;
 public class CommitTest {
     public static void main(String[] args) {
         System.out.println("添加新的东西！");
+        System.out.println("菜鸟添加的新内容");
     }
 }
